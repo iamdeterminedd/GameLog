16c914471e25ee68ac37033c7bda9b1431776f10
cryptorJim <jimcyril.martinez@gmail.com>
2023-11-02 09:35:03 +0800

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,15 @@
 import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/game-log-b.png';
+import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 import React from 'react';
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput onSearch={''} />
+      <ColorModeSwitch />
     </HStack>
   );
 };
