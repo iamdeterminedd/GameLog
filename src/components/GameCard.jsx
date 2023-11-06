ca98c7c8eddf51a5abcd730ba433ea5008f0c9c9
cryptorJim <jimcyril.martinez@gmail.com>
2023-11-07 07:37:40 +0800

    create gamecardcontainer

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -4,7 +4,7 @@ import CriticScore from './CriticScore';
 
 const GameCard = ({ game }) => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
+    <Card>
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
---
+++ src/components/GameCardContainer.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+const GameCardContainer = ({ children }) => {
+  return (
+    <Box width="300px" borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
