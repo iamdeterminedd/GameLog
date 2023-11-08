29221228c7cf14cb0fba66e99aca8f69d00200d2
cryptorJim <jimcyril.martinez@gmail.com>
2023-11-08 13:44:32 +0800

    platform selector dropdown button

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './styles.css';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
 import { useState } from 'react';
+import PlatformSelector from './components/PlatformSelector';
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState(null);
@@ -32,6 +33,7 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area="main">
+          <PlatformSelector />
           <GameGrid selectedGenre={selectedGenre} />
         </GridItem>
       </Grid>
