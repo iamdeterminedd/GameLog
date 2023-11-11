901ee71c32833575db9566cf5eb00af2c715048f
cryptorJim <jimcyril.martinez@gmail.com>
2023-11-12 07:30:23 +0800

    fetching genre using react query

--- src/hooks/useGenres.js
+++ src/hooks/useGenres.js
@@ -1,5 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '../services/api-client';
 import genre from '../data/genre';
 
-const useGenres = () => ({ data: genre, isLoading: false, error: null });
+const useGenres = () =>
+  useQuery({
+    queryKey: ['genres'],
+    queryFn: () => apiClient.get('/genres').then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 100,
+    initialData: { count: genre.length, results: genre },
+  });
 
 export default useGenres;
